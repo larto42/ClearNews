41f3c024d4257d538596d02028ecfe5e7a033227
Konrad Nojman <94konrad.n@gmail.com>
2019-08-04 00:40:46 +0200

    added recompose to project

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import LoginForm from "../components/LoginForm/LoginForm";
 import { withFirebase } from "../utils/Firebase";
 import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
 
-const LoginFormConnected = withRouter(withFirebase(LoginForm));
+const LoginFormConnected = compose(
+	withRouter,
+	withFirebase
+)(LoginForm);
 
 export default function LoginPage() {
 	return (
