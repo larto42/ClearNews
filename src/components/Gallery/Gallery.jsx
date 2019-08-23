22f8ce480d7cf5005b73c6f487dbceaf00531247
Konrad Nojman <94konrad.n@gmail.com>
2019-08-23 21:04:19 +0200

    Add swipe animation for gallery.

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import SwipeListener from "swipe-listener";
 
 export default class Gallery extends Component {
 	state = {
@@ -18,15 +19,60 @@ export default class Gallery extends Component {
 				url: "https://source.unsplash.com/random/?peace",
 				alt: "random img"
 			}
-		]
+		],
+		currImg: 0
+	};
+
+	picturesRef = React.createRef();
+
+	componentDidMount = () => {
+		const listener = SwipeListener(this.picturesRef.current);
+		console.log("picture ref", this.picturesRef);
+		this.picturesRef.current.addEventListener("swipe", e => {
+			const { left, right } = e.detail.directions;
+			const { currImg } = this.state;
+			console.log("curr img przed zmiana", currImg);
+			const len = this.state.images.length - 1;
+			let index = 0;
+			if (left) {
+				index = currImg + 1 > len ? len : currImg + 1;
+			} else {
+				if (right) {
+					index = currImg - 1 < 0 ? 0 : currImg - 1;
+				}
+			}
+
+			this.setState({
+				currImg: index
+			});
+
+			console.log(left, right, currImg);
+		});
+	};
+
+	componentWillUnmount = () => {
+		this.picturesRef.current.removeEventListener("swipe");
 	};
 
 	render() {
+		const { images, currImg } = this.state;
+		const imgCount = images.length;
+		const transform = 100 / imgCount;
+
+		const galeryTransformStyle = {
+			transform: `translateX(calc(-${transform * currImg}% - ${10 *
+				currImg}px))`
+		};
+
 		return (
 			<section className="gallery">
 				<h2 className="gallery__title">In pictures</h2>
 				<div className="pictures">
-					<div className="pictures__wrapper">
+					<div
+						ref={this.picturesRef}
+						className="pictures__wrapper"
+						style={galeryTransformStyle}
+					>
 						{this.state.images.map(item => (
 							<picture key={item.id} className="pictures__item">
 								<img
